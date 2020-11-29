5f5c1acce78b7ec4ed51cd9be1d5dae54e18ecbf
Tim <lobanovtim@gmail.com>
2020-11-29 17:24:33 +0300

    homeWork6
    Со всем разобрался, кроме того, как сравнить кнопоку удара и получить её значение max/minDamage . Лучше дальше пытаться перебирать массив или evt.target использовать? 3 раза приступал к задаче, пока не получилось

---
+++ renderLog.js
@@ -0,0 +1,57 @@
+import { player1, player2, allButtons, gameOver } from "./main.js";
+import random from "./utils.js";
+
+const $logs = document.querySelector('#logs')
+const logArr = [];
+
+
+const deleteButton = () =>{
+    allButtons.forEach(item => {
+        item.remove();
+    });
+}
+
+const renderLog = (player, count) => {
+    let log = player === player1 ? generateLog(player1, player2, count) : generateLog(player2, player1, count);
+    logArr.push(log);
+    const $p = document.createElement('p');
+    $p.innerText = `${log}`;  
+    $logs.insertBefore($p, $logs.children[0]);
+    console.log(logArr);
+    if (player1.hp.current < count && player2.hp.current < count) {
+        $p.innerText = `Бой был равный! Ничья.`
+        deleteButton();
+        gameOver();
+    }
+    if (player.hp.current <= count) {
+        player.hp.current = 0;
+        $p.innerText = `Бедный ${player.name} проиграл бой!`;
+        $logs.insertBefore($p, $logs.children[0]);
+        deleteButton();
+        gameOver();
+    }
+
+}
+
+function generateLog(player1, player2, count) {
+    const { name, hp: {current, total} } = player1;
+    const { name: enemyName } = player2;
+
+
+    const logs = [
+        `${name} вспомнил что-то важное, но неожиданно ${enemyName}, не помня себя от испуга, ударил в предплечье враг. -${count}HP[${current}/${total}]`, 
+        `${name} поперхнулся, и за это ${enemyName} с испугу приложил прямой удар коленом в лоб врага. -${count}HP[${current}/${total}]`,
+        `${name} забылся, но в это время наглый ${enemyName}, приняв волевое решение, неслышно подойдя сзади, ударил. -${count}HP[${current}/${total}]`,
+        `${name} пришел в себя, но неожиданно ${enemyName} случайно нанес мощнейший удар. -${count}HP[${current}/${total}]`,
+        `${name} поперхнулся, но в это время ${enemyName} нехотя раздробил кулаком \<вырезанно цензурой\> противника. -${count}HP[${current}/${total}]`,
+        `${name} удивился, а ${enemyName} пошатнувшись влепил подлый удар. -${count}HP[${current}/${total}]`,
+        `${name} высморкался, но неожиданно ${enemyName} провел дробящий удар. -${count}HP[${current}/${total}]`,
+        `${name} пошатнулся, и внезапно наглый ${enemyName} беспричинно ударил в ногу противника. -${count}HP[${current}/${total}]`,
+        `${name} расстроился, как вдруг, неожиданно ${enemyName} случайно влепил стопой в живот соперника. -${count}HP[${current}/${total}]`,
+        `${name} пытался что-то сказать, но вдруг, неожиданно ${enemyName} со скуки, разбил бровь сопернику. -${count}HP[${current}/${total}]`
+    ];
+    
+    return logs[random(0, logs.length)];
+};
+
+export default renderLog;
