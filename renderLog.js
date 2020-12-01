6b05ec43aa2529b4edd6455150eed461969cc72f
Tim <lobanovtim@gmail.com>
2020-12-01 21:00:57 +0300

    Получает покемонов с сервера

--- renderLog.js
+++ renderLog.js
@@ -1,9 +1,10 @@
-import { player1, player2, allButtons, gameOver } from "./main.js";
+import game from "./main.js";
 import random from "./utils.js";
 
 const $logs = document.querySelector('#logs')
 const logArr = [];
 
+// console.log(game)
 
 const deleteButton = () =>{
     allButtons.forEach(item => {
@@ -11,13 +12,20 @@ const deleteButton = () =>{
     });
 }
 
+const clearLog = () => {
+    logArr.forEach(item => {
+        item.remove();
+    });
+}
+
+
 const renderLog = (player, count) => {
-    let log = player === player1 ? generateLog(player1, player2, count) : generateLog(player2, player1, count);
+    let log = player === game.player1 ? generateLog(game.player1, game.player2, count) : generateLog(game.player2, game.player1, count);
     logArr.push(log);
     const $p = document.createElement('p');
     $p.innerText = `${log}`;  
     $logs.insertBefore($p, $logs.children[0]);
-    console.log(logArr);
+    // console.log(logArr);
     if (player1.hp.current < count && player2.hp.current < count) {
         $p.innerText = `Бой был равный! Ничья.`
         deleteButton();
@@ -29,11 +37,14 @@ const renderLog = (player, count) => {
         $logs.insertBefore($p, $logs.children[0]);
         deleteButton();
         gameOver();
+        // console.log(logArr);
+        // clearLog();
     }
 
 }
 
 function generateLog(player1, player2, count) {
+    console.log(player1)
     const { name, hp: {current, total} } = player1;
     const { name: enemyName } = player2;
 
